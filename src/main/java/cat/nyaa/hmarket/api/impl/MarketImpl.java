b04738e16a7bf44917676d359d4e169dc9e16f39
Takaranoao <takaranoao@gmail.com>
2022-05-09 17:18:20 +0800

    fix(sold message): total price

--- src/main/java/cat/nyaa/hmarket/api/impl/MarketImpl.java
+++ src/main/java/cat/nyaa/hmarket/api/impl/MarketImpl.java
@@ -204,7 +204,7 @@ public class MarketImpl implements IMarketAPI {
                             HMLogUtils.logWarning("from: " + playerId);
                         }
                     }
-                    onShopSold(player, shopItemData, itemStack, amount);
+                    onShopSold(player, shopItemData, itemStack, amount, paidCost.get(), paidTax.get());
                     HMLogUtils.logInfo("Player " + playerId + " bought " + itemStack + " from market " + marketId);
                     HMLogUtils.logInfo("cost: " + paidCost.get() + " tax: " + paidTax.get() + "," + shopItemData.owner() + " received: " + (paidCost.get() - paidTax.get()));
                     return true;
@@ -249,7 +249,7 @@ public class MarketImpl implements IMarketAPI {
 
     }
 
-    private void onShopSold(Player player, ShopItemData shopItemData, ItemStack itemStack, int amount) {
+    private void onShopSold(Player player, ShopItemData shopItemData, ItemStack itemStack, int amount, double paidCost, double paidTax) {
         AoMessage.getInstanceOptional().ifPresent(
                 aoMessage -> aoMessage.sendMessageTo(
                         player.getUniqueId(),
@@ -262,7 +262,7 @@ public class MarketImpl implements IMarketAPI {
 
                                 amount,
                                 player.getName(),
-                                shopItemData.price() * amount
+                                paidCost + paidTax
                         )
                 )
         );
